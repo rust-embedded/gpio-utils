38398bfcacbcb5374c2095bf6b5be40c6967c791
Paul Osborne <osbpau@gmail.com>
2016-04-23 23:34:08 -0500

    license: fixup ownership in license headers
    The copyright for files is shared between all the developers
    that have contributed to the project, not just me.  This
    change does not modify the license itself at all.
    
    Signed-off-by: Paul Osborne <osbpau@gmail.com>

--- src/commands/gpio_read.rs
+++ src/commands/gpio_read.rs
@@ -1,4 +1,10 @@
-// Copyright (C) 2016, Paul Osborne <osbpau@gmail.com>
+// Copyright (c) 2016, The gpio-utils Authors.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/license/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option.  This file may not be copied, modified, or distributed
+// except according to those terms.
 
 use options::GpioReadOptions;
 use config::GpioConfig;
